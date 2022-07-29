bbf6502044c030ee97938395fbcac0119eb5f01a
rutuja_mali <rutujamali111@gmail.com>
2022-07-30 02:40:21 +0530

    Insert method implementation

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -39,6 +39,12 @@ public class MyLinkedList {
 			this.tail=myNode;
 		}
 	}
+	public void insert(INode myNode,INode newNode)
+	{
+		INode tempNode=myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
 	public void PrintMyNodes()
 	{
 		StringBuffer myNodes=new StringBuffer("My nodes: ");
--- MyNodeTest.java
+++ MyNodeTest.java
@@ -6,6 +6,7 @@ public class MyNodeTest {
 		MyNode<Integer> myFirstNode=new MyNode<>(56);
 		MyNode<Integer> mySecondNode=new MyNode<>(30);
 		MyNode<Integer> myThirdNode=new MyNode<>(70);
+		MyNode<Integer> myforthNode=new MyNode<>(40);
 		System.out.println(myFirstNode.getKey());
 		System.out.println(mySecondNode.getKey());
 		System.out.println(myThirdNode.getKey());
@@ -13,6 +14,8 @@ public class MyNodeTest {
 		myLinkedList.append(myFirstNode);
 		myLinkedList.append(mySecondNode);
 		myLinkedList.append(myThirdNode);
+		myLinkedList.PrintMyNodes();
+		myLinkedList.insert(myFirstNode,myforthNode);
 		myLinkedList.PrintMyNodes();
 		
 		
