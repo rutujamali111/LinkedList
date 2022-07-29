4c2b342d8d3f96afcf0ec42380f5949e3d200016
rutuja_mali <rutujamali111@gmail.com>
2022-07-30 01:31:15 +0530

    Creating a simple nodes

---
+++ MyNodeTest.java
@@ -0,0 +1,16 @@
+
+public class MyNodeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyNode<Integer> myFirstNode=new MyNode<>(56);
+		MyNode<Integer> mySecondNode=new MyNode<>(30);
+		MyNode<Integer> myThirdNode=new MyNode<>(70);
+		System.out.println(myFirstNode.getKey());
+		System.out.println(mySecondNode.getKey());
+		System.out.println(myThirdNode.getKey());
+		
+
+	}
+
+}
